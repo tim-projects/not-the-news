c98818c5d313b057a04eb62003a31c7a914666f7
Tim <Tim>
2025-07-19 10:53:39 +0700

    sw.js cache files

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,33 @@
+const cacheName = 'not-the-news-v1';
+const cacheAssets = [
+  'index.html',
+  'login.html',
+  'style.css',
+  'js/app.js',
+  'js/data/appState.js',
+  'js/data/database.js',
+  'js/helpers/apiUtils.js',
+  'js/helpers/dataUtils.js',
+  'js/helpers/userStateUtils.js',
+  'js/ui/uiElements.js',
+  'js/ui/uiInitializers.js',
+  'js/ui/uiUpdaters.js',
+  'libs/alpine.3.x.x.js',
+  'libs/idb.js',
+  'libs/rss-parser.min.js',
+  'fonts/Playfair_Display.ttf',
+  'images/placeholder.svg'
+];
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(cacheAssets);
+      })
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,5 +36,15 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
