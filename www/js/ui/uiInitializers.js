c220a2b64f84040d38405d7021a56e0e9b8f09e9
Tim <Tim>
2025-07-28 17:57:56 +0700

    improve shuffleCount functionality

--- www/js/ui/uiInitializers.js
+++ www/js/ui/uiInitializers.js
@@ -12,7 +12,7 @@ import {
 } from '../data/database.js';
 
 import { getSyncToggle, getSyncText, getImagesToggle, getImagesText, getThemeToggle, getThemeText, getShuffleCountDisplay, getMainSettingsBlock, getRssSettingsBlock, getKeywordsSettingsBlock, getBackButton, getRssFeedsTextarea, getKeywordsBlacklistTextarea, getConfigureRssButton, getConfigureKeywordsButton, getSaveKeywordsButton, getSaveRssButton } from './uiElements.js';
-import { loadShuffleState, saveShuffleState } from '../data/database.js'; // Ensure imports are correct
+import { loadShuffleState, saveShuffleState } from '../data/uiInitializers.js';
 import { loadConfigFile, saveConfigFile } from '../helpers/apiUtils.js';
 import { createStatusBarMessage, attachScrollToTopHandler } from './uiUpdaters.js';
 
