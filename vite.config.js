ebd959d511b3b663aee1924badd0ca33cd0ac4f7
Tim <Tim>
2025-08-01 11:46:54 +0700

    switch to vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  // This tells Vite that the 'src' directory is the root of your source files.
-  root: 'src',
   build: {
-    // This tells Vite to put the output one directory up from the root (`src`).
-    outDir: '../www',
-    emptyOutDir: true, // Clean the output directory before building
-    // This tells Vite to process all three files as separate entry points.
+    // We can revert to a simple outDir since the input paths are absolute.
+    outDir: 'www',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'index.html',
-        login: 'login.html',
-        sw: 'sw.js',
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login.html'),
+        sw: resolve(__dirname, 'src/sw.js'),
       },
     },
   },
