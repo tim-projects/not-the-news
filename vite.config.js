c2bccc8b031bcfc5b849f63718635654b7a84112
Tim <Tim>
2025-08-01 11:43:11 +0700

    switch to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // This tells Vite that the 'src' directory is the root of your source files.
+  root: 'src',
   build: {
-    outDir: 'www',
+    // This tells Vite to put the output in a 'www' directory that is outside
+    // the 'src' directory (relative to the project root).
+    outDir: '../www',
+    emptyOutDir: true, // Clean the output directory before building
   }
 });
