39040846d11daacaa91db501ada5d6a020c1e78f
Tim <Tim>
2025-08-01 12:09:00 +0700

    switch to vite

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,18 @@ export default defineConfig({
         login: resolve(__dirname, 'src/login.html'),
         sw: resolve(__dirname, 'src/sw.js'),
       },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          // Force sw.js to be named sw.js and placed in the root
+          if (chunkInfo.name === 'sw') {
+            return '[name].js';
+          }
+          // The other files will get standard hashed names.
+          return 'assets/[name]-[hash].js';
+        },
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
     },
   },
 });
