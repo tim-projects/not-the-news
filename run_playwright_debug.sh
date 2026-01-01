2c522710be6a81d060fcbb131699482ba19a7420
bitmango-trading <bitmango-trading@bitmango.com>
2026-01-01 09:47:36 +0000

    chore: remove legacy scripts and move log files to logs/ directory

--- run_playwright_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export APP_URL=http://localhost:8080
-
-podman exec -e APP_URL ntn-dev bash -c "npx playwright test tests/ui.spec.js > /tmp/playwright_test_output.log 2>&1"
-
-# Download test results after the playwright tests have finished
-bash download_test_results.sh
